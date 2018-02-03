20d8fcff0c7d0e51f5a34892db29a397197e5d38
ebiiim <mail@ebiiim.com>
2018-02-04 00:40:33 +0900

    コメント

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ SECRET = toml.load(open('secret.toml', encoding='utf-8'))
 # 認証後に代入する
 bot_id = None
 
-# a handler which displays logs to console
+# A handler which displays logs to console
 logging.getLogger().setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
@@ -22,6 +22,11 @@ sc = SlackClient(SECRET['slack']['api_token'])
 
 
 def handle_cmd_kill(cmd):
+    """
+    killコマンドの処理を行うメソッド
+    :param cmd: botにメンションで送ったメッセージ
+    :return: Slackに投稿するメッセージ
+    """
     cmds = cmd.split()
     if len(cmds) > 1:
         if cmds[1] in ['you', 'お前']:
@@ -30,22 +35,33 @@ def handle_cmd_kill(cmd):
 
 
 def handle_command(cmd, channel):
+    """
+    botに対するメンションを処理するメソッド, コマンドの識別および処理の実施を行う
+    :param cmd: コマンド(botに対するメンション)
+    :param channel: Slackの対象チャンネル
+    :return: サーバの応答(Slackの対象チャンネルにコマンドの実行結果を投稿した結果)
+    """
     response = CONFIG['bot']['res_default']
-
+    print(cmd)
     if cmd.startswith(('help', '-h', '--help', '使い方', 'a')):
         response = CONFIG['bot']['res_help']
     elif cmd.startswith('kill'):
         response = handle_cmd_kill(cmd)
 
-    sc.api_call('chat.postMessage', channel=channel, text=response, as_user=True)
+    return sc.api_call('chat.postMessage', channel=channel, text=response, as_user=True)
 
 
 def parse_slack_cmd(cmd):
+    """
+    RTM(Real Time Messaging) APIで受け取ったメッセージをパースするメソッド, botに対するメンションのみを認識する
+    :param cmd: RTMで受け取ったJSON
+    :return: (botに対するメンション, 対象チャンネル)
+    """
     at_bot = '<@' + bot_id + '>'
     for elem in cmd:
         logging.debug("Parsing: " + str(cmd))
         if ('text' in elem) and (at_bot in elem['text']):
-            return elem['text'].split(at_bot)[1], elem['channel']
+            return elem['text'].split(at_bot)[1].strip(), elem['channel']
     return None, None
 
 
@@ -55,10 +71,10 @@ if __name__ == '__main__':
         logging.info('Bot connected and running!')
         # Read bot's user ID by calling Web API method <auth.test>
         bot_id = sc.api_call("auth.test")["user_id"]
-        while True:
+        while True:  # コマンド待ち
             cmd, channel = parse_slack_cmd(sc.rtm_read())
             if cmd and channel:
-                handle_command(cmd, channel)
+                logging.info(str(handle_command(cmd, channel)))
             time.sleep(RTM_READ_DELAY)
     else:
         logging.warning('Connection failed.')
