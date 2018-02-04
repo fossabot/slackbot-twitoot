7f0ffe2a20d34f5bbda0f32f34a4a30a781eb05d
ebiiim <mail@ebiiim.com>
2018-02-04 18:25:47 +0900

    twitter用のtomlフォーマットを変更

--- tweetest.py
+++ tweetest.py
@@ -4,11 +4,11 @@ import toml
 SECRET = toml.load(open('secret.toml', encoding='utf-8'))
 CONFIG = toml.load(open('config.toml', encoding='utf-8'))
 
-for account in ['twitter_1']:  # 複数アカウント対応
-    CK = SECRET[account]['consumer_key']
-    CS = SECRET[account]['consumer_secret']
-    AT = SECRET[account]['access_token']
-    AS = SECRET[account]['access_token_secret']
+for account in ['id_'+ str(i+1) for i in range(1)]:  # 複数アカウント対応
+    CK = SECRET['twitter']['app_1']['consumer_key']
+    CS = SECRET['twitter']['app_1']['consumer_secret']
+    AT = SECRET['twitter']['app_1'][account]['access_token']
+    AS = SECRET['twitter']['app_1'][account]['access_token_secret']
 
     twitter = OAuth1Session(CK, CS, AT, AS)
 
