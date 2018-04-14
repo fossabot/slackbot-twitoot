4212a9580b8148e91f58c7aaffdec9897baa345d
ebiiim <mail@ebiiim.com>
2018-04-14 15:56:08 +0900

    fix

--- cmd.sh
+++ cmd.sh
@@ -9,7 +9,7 @@ sed -i -e "s/oauth_token = ''/oauth_token = '$OAUTH_TOKEN'/" secret.toml
 sed -i -e "s/bot_token = ''/bot_token = '$BOT_TOKEN'/" secret.toml
 sed -i -e "s/consumer_key = ''/consumer_key = '$TWITTER_CONSUMER_KEY'/" secret.toml
 sed -i -e "s/consumer_secret = ''/consumer_secret = '$TWITTER_CONSUMER_SECRET'/" secret.toml
-sed -i -e "s/access_token = ''/access_token = '$TWITTER_ACCESS_TOKEN'/" secret.toml
+sed -i -e "1s/access_token = ''/access_token = '$TWITTER_ACCESS_TOKEN'/" secret.toml
 sed -i -e "s/access_token_secret = ''/access_token_secret = '$TWITTER_ACCESS_TOKEN_SECRET'/" secret.toml
 sed -i -e "s#url = ''#url = '$MASTODON_URL'#" secret.toml
 sed -i -e "s/client_key = ''/client_key = '$MASTODON_CLIENT_KEY'/" secret.toml
